9bae44e3319999cb43bdb1af7e2cd23f6d3cd15d
Yuri <dog-pit10@yandex.ru>
2021-03-31 12:41:16 +0300

    modified code...added logic

---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,64 @@
+import React, {Component} from "react";
+import "./post-list-item.css";
+
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+            like: false
+        };
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+        const {label} = this.props;
+        const {important, like} = this.state;
+        let className = 'app-list-item d-flex justify-content-between'
+
+        if (important) {
+            className += ' important';
+        }
+
+        if (like) {
+            className += ' like';
+        }
+        return (
+            <div className={className}>
+            <span onClick={this.onLike} className={'app-list-item-label'}>
+                {label}
+            </span>
+                <div className={'d-flex justify-content-center align-items-center'}>
+                    <button
+                        type={'submit'}
+                        onClick={this.onImportant}
+                        className={'btn-star btn-sm'}>
+                        <i className={'fa fa-star'}>
+                        </i>
+                    </button>
+                    <button
+                        type={'submit'}
+                        className={'btn-trash btn-sm'}>
+                        <i className={'fa fa-trash-o'}>
+                        </i>
+                    </button>
+                    <i className={'fa fa-heart'}>
+                    </i>
+                </div>
+            </div>
+        )
+    }
+}
